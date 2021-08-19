6ece5cf1eea8903a3863de90311e9c65e127fda4
TylerLadas <tylerladas@hotmail.com>
2021-08-19 13:22:50 -0400

    add user routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const exphbs = require('express-handlebars');
 // Required folders
 // const helpers = require('./utils/helpers');
 const path = require('path');
+const controllers = require('./controllers')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -34,7 +35,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+// turn on controllers
+app.use(controllers);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
